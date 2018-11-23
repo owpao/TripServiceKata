cb04460b1ab7601b2accff8827a111d5db2fad50
John Paolo Flores <john.paolo.flores@oocl.com>
2018-11-23 11:58:49 +0800

    Added test cases in User Class; vid ended at 40:24

--- src/test/java/org/craftedsw/tripservicekata/user/UserTest.java
+++ src/test/java/org/craftedsw/tripservicekata/user/UserTest.java
@@ -1,5 +1,33 @@
 package org.craftedsw.tripservicekata.user;
 
-public class UserTest {
+import org.craftedsw.tripservicekata.trip.UserBuilder;
+import org.junit.Assert.*;
+import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class UserTest {
+    private static final User PAO = new User();
+    private static final User JOBERT = new User();
+
+    @Test
+    public void
+    shouldInformWhenUsersAreNotFriends(){
+        User user = UserBuilder.aUser()
+                                .friendsWith(PAO)
+                                .build();
+
+        assertThat(user.isFriendsWith(JOBERT), is(false));
+    }
+
+    @Test
+    public void
+    shouldInformWhenUsersAreFriends(){
+        User user = UserBuilder.aUser()
+                .friendsWith(PAO, JOBERT)
+                .build();
+
+        assertThat(user.isFriendsWith(JOBERT), is(true));
+    }
 }
